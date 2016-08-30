160b86a3bb2b1a7f6fcb0111b6cc8261415396d7
John Gilbertson <ja.gilbertson1@gmail.com>
2016-08-30 16:51:38 +1000

    Better way to get data directory that works on windows

--- Nearest Neighbour Classifier/nearestNeighbour - predict.py
+++ Nearest Neighbour Classifier/nearestNeighbour - predict.py
@@ -1,13 +1,18 @@
 import csv
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+import os
+
+
+train_path = os.path.join(os.getcwd(), "..", "data", "train.csv")
+test_path = os.path.join(os.getcwd(), "..", "data", "test.csv")
 
 # import full training data set
-with open('../data/train.csv', 'rb') as csvfile:
+with open(train_path, 'rb') as csvfile:
     dataTrainImport = np.loadtxt(csvfile, delimiter=',')
 
 # import full test data set
-with open('../data/test.csv', 'rb') as csvfile:
+with open(test_path, 'rb') as csvfile:
     dataTestImport = np.loadtxt(csvfile, delimiter=',')
 
 # extract bitmaps and character annotation
@@ -18,7 +23,7 @@ dataTest = dataTestImport[:, 9:439]
 idsTest = dataTestImport[:, 0]
 
 # number of neighbours from model selection
-k = 8
+k = 9
 
 # fit nearest neighbours classifier (with L2-norm)
 neigh = KNeighborsClassifier(n_neighbors = k, weights = 'uniform', algorithm = 'auto', p = 2)
