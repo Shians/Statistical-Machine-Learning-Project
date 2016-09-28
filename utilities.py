b40548896108aca5254956fd6cc37f24f91e2b4a
shian_su <registertonysu@gmail.com>
2016-09-28 12:49:08 +1000

    - Updated utilities.py

--- utilities.py
+++ utilities.py
@@ -6,9 +6,14 @@ def show_char(data, index):
     plt.imshow(data[index, 9:439].reshape([33,13]), cmap='Greys_r')
     
 # Shows the images of char from data, up to number of images shown
-def show_chars_like(data, char, number):
-    selected_data = np.array([x for x in data if x[1] == char])
+def show_chars_like(data, char, number, book=-1):
+    if book == -1:
+        selected_data = random.permutation([x for x in data if x[1] == char])
+    else:
+        selected_data = random.permutation([x for x in data if x[1] == char and x[2] == book])
+        
     entries = min(number, selected_data.shape[0])
+    print(entries)
     for i in range(entries):
         show_char(selected_data, i)
         plt.show()
