6cc7c5f779f93b5b7abd30bd0ac1af2f5fd6dd02
John Alexander Gilbertson <johnag@student.unimelb.edu.au>
2016-10-03 11:18:53 +1100

    Python script to calculate cross validation error for a k up to 25 and append to previously calculated results

--- Nearest Neighbour Classifier/nearestNeighbour_CV.py
+++ Nearest Neighbour Classifier/nearestNeighbour_CV.py
@@ -17,7 +17,8 @@ else:
     with open(small_train_path, 'rb') as csvfile:
         dataImport = np.loadtxt(csvfile, delimiter=',')
 
-param_grid = {'n_neighbors' : np.arange(1, 16, 1), 'p' : np.arange(1, 4, 1)}
+# param_grid = {'n_neighbors' : np.arange(1, 16, 1), 'p' : np.arange(1, 4, 1)}
+param_grid = {'n_neighbors' : np.arange(16, 26, 1), 'p' : np.arange(1, 4, 1)}
 
 neigh = KNeighborsClassifier(weights = 'uniform', algorithm = 'auto')
 
@@ -29,10 +30,14 @@ numNeighbors = [val.parameters['n_neighbors'] for val in results.grid_scores_]
 pNorm = [val.parameters['p'] for val in results.grid_scores_]
 scores = [val.mean_validation_score for val in results.grid_scores_]
 
-# delete output file if exists
-if os.path.exists('nearestNeighbour_CV.csv'):
-    os.remove('nearestNeighbour_CV.csv')
+for i in range(len(numNeighbors)):
+	with open('nearestNeighbour_CV.csv', 'a') as outputFile:
+		outputFile.write(str(numNeighbors[i]) + "," + str(pNorm[i]) + "," + str(scores[i]) + "\n")
+
+# # delete output file if exists
+# if os.path.exists('nearestNeighbour_CV.csv'):
+#     os.remove('nearestNeighbour_CV.csv')
     
-output = np.column_stack((numNeighbors, pNorm, scores))
-np.savetxt('nearestNeighbour_CV.csv', output, fmt='%f',
-           header="n_neighbors,p,cross_validation_accuracy", delimiter=",", comments="")
+# output = np.column_stack((numNeighbors, pNorm, scores))
+# np.savetxt('nearestNeighbour_CV.csv', output, fmt='%f',
+#            header="n_neighbors,p,cross_validation_accuracy", delimiter=",", comments="")
